2338844162a2e6cf5536de7965ec340139e6ac66
Cheapo587 <ac.shultz587@gmail.com>
2017-10-03 23:38:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+//write your code below!
+
+function happyHolidays(){
+  return "Happy holidays!"
+}
+
+function happyHolidaysTo(string){
+  return `Happy Holidays, ${string}!`
+}
+
+function happyHolidayTo(string holiday, string name){
+  return `Happy ${holiday}, ${name}!`
+}
+
+function holidayCountdown(string holiday, string count){
+  return `It's ${count} days until ${holiday}!`
+}
